1e6b7cf7c13191782adeb76b9d6e3af1d903fd99
wenpeng jiang <monashboxer@gmail.com>
2020-07-01 23:25:14 +0800

    在表格中显示数据

--- vueblog/src/views/edu/teacher/list.vue
+++ vueblog/src/views/edu/teacher/list.vue
@@ -1,5 +1,57 @@
 <template>
-  <div class="app-container">讲师列表</div>
+  <div class="app-container">
+    讲师列表
+    <!-- 表格 -->
+
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="数据加载中"
+      border
+      fit
+      highlight-current-row
+    >
+      <el-table-column label="序号" width="70" align="center">
+        <template slot-scope="scope">{{ (page - 1) * limit + scope.$index + 1 }}</template>
+      </el-table-column>
+
+      <el-table-column prop="name" label="名称" width="80" />
+
+      <el-table-column label="头衔" width="80">
+        <template slot-scope="scope">{{ scope.row.level===1?'高级讲师':'首席讲师' }}</template>
+      </el-table-column>
+
+      <el-table-column prop="intro" label="资历" />
+
+      <el-table-column prop="gmtCreate" label="添加时间" width="160" />
+
+      <el-table-column prop="sort" label="排序" width="60" />
+
+      <el-table-column label="操作" width="200" align="center">
+        <template slot-scope="scope">
+          <router-link :to="'/edu/teacher/edit/'+scope.row.id">
+            <el-button type="primary" size="mini" icon="el-icon-edit">修改</el-button>
+          </router-link>
+          <el-button
+            type="danger"
+            size="mini"
+            icon="el-icon-delete"
+            @click="removeDataById(scope.row.id)"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <!-- 分页 -->
+    <el-pagination
+      :current-page="page"
+      :page-size="limit"
+      :total="total"
+      style="padding: 30px 0; text-align: center;"
+      layout="total, prev, pager, next, jumper"
+      @current-change="fetchData"
+    />
+  </div>
 </template>
 
 <script>
@@ -15,7 +67,7 @@ export default {
     };
   },
   created() {
-      this.getList();
+    this.getList();
   },
   methods: {
     getList() {
@@ -23,6 +75,8 @@ export default {
         .getTeacherListPage(this.page, this.limit, this.teacherQuery)
         .then(response => {
           //请求成功
+          this.list = response.data.rows
+          this.total = response.data.total
           console.log(response);
         })
         .catch(error => {
